02e2df87d3f26cc9fa30b9465a4391d01e3d42ba
Prasoon <prasoonr@zeta.tech>
2024-01-13 10:42:46 +0530

    Initial Commit

---
+++ todoListStoryBorard/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  todoListStoryBorard
+//
+//  Created by Prasoon.r on 13/01/24.
+//
+
+import Foundation
+
+struct Todo{
+    let title: String
+    let isComplelte: Bool = false
+    let temp: Bool = false
+}
+
+
---
+++ todoListStoryBorard/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  todoListStoryBorard
+//
+//  Created by Prasoon.r on 13/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let Todos = [
+        Todo(title: "Todo1"),
+        Todo(title: "Todo2"),
+        Todo(title: "Todo3"),
+    ]
+    
+    
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
